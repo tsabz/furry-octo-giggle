0440b3cc12eb1f70902b46138b58a0d230349bb7
Madeline Dutro <dutro.madeline@gmail.com>
2020-06-20 11:13:05 -0400

    adds signup functionality to page, and preps for the rest of auth

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,21 @@
-cont app = angular.module("CommonplaceApp", []);
+const app = angular.module("CommonplaceApp", []);
 
 app.controller("CommonplaceController", ['$http', function($http) {
+  this.loggedInUser = false
+  this.createForm = {}
+  this.signup = () => {
+    $http({
+      url: '/user',
+      method: 'POST',
+      data: this.createForm
+    }).then((response) => {
+      console.log(response.data)
+      this.loggedInUser = response.data
+      this.createForm = {}
+    })
+  }
+  this.login = () => {
+    
+  }
 
 }]); //BEYOND THE WALL
