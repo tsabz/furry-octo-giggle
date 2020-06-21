8b7e717459d02c4a9d02ff38b557a2c07115798c
Hannah Livnat <hannahlivnat@gmail.com>
2020-06-21 13:09:13 -0700

    edits made to form page

--- public/js/style.js
+++ public/js/style.js
@@ -1,1 +1,9 @@
-console.log($);
+$(() => {
+  //dynamically update text fields in input type text
+  M.updateTextFields();
+
+
+  //dynamically change text area for edit form
+  $('#edit-textarea').val('New Text');
+  M.textareaAutoResize($('#edit-textarea'));
+})
